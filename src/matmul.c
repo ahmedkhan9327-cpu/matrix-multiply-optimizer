a8ba5c66ffc6a7504ef78b950d284be5834f77b6
ahmedkhan9327-cpu <ahmed.khan9327@gmail.com>
2026-06-15 22:28:57 -0700

    matmul: naive implementation reorder loops i-k-k

--- src/matmul.c
+++ src/matmul.c
@@ -22,8 +22,8 @@ int main(void) {
     clock_gettime(CLOCK_MONOTONIC, &start);
 
     for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            for (int k = 0; k < N; k++) {
+        for (int k = 0; k < N; k++) {
+            for (int j = 0; j < N; j++) {
                 C[i][j] += A[i][k] * B[k][j];
             }
         }
